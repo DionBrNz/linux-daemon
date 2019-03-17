8f996cd5427a392fcabf371fe3d95db0a8ac214a
Dion Brown <dion.brown@outlook.co.nz>
2019-03-17 19:06:31 +1300

    Initial check in.

---
+++ linux-daemon/DaemonConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace linux_daemon
+{
+    internal class DaemonConfig
+    {
+        public string DaemonName { get; set; } = "Test-Daemon";
+    }
+}
---
+++ linux-daemon/DaemonService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace linux_daemon
+{
+    internal class DaemonService : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private readonly IOptions<DaemonConfig> _config;
+        private readonly Timer _timer;
+        public DaemonService(IOptions<DaemonConfig> config, ILogger<DaemonService> logger)
+        {
+            _config = config;
+            _logger = logger;
+            _timer = new Timer(TimerProc);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting daemon: " + _config.Value.DaemonName);
+            _timer.Change(100, 1000);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping daemon.");
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _logger.LogInformation("Disposing....");
+        }
+
+        private void TimerProc(object state)
+        {
+            _logger.LogInformation("Deamon running");
+        }
+    }
+}
